8df303bde286960a8f45c2f343d3f8b7d612d4e3
Carrasco Romel <romelx23@gmail.com>
2022-03-29 21:36:30 -0500

    map production

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import * as mapboxgl from 'mapbox-gl';
-import { environment } from '../environments/environment.prod';
+import { environment } from '../environments/environment';
 
 @Component({
   selector: 'app-root',
